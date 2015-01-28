42289147b5845ff77eba3828bbdc035c1732defd
mrseasons <mrseasons@mrseasonsdeMacBook-Pro.local>
2015-01-28 08:16:21 +0800

    update package

---
+++ coffeetime-parent/coffeetime-kotlin/src/main/kotlin/org/mrseasons/coffeetime/kotlin/Greeting.kt
@@ -0,0 +1,10 @@
+package org.mrseasons.coffeetime.kotlin
+
+/**
+ * A function to say hello to given user.
+ *
+ * @param name user name, optional
+ * @return greeting string to given user
+ *
+ */
+public fun sayHi(name: String = "World"): String = "Hello $name!"
---
+++ coffeetime-parent/coffeetime-kotlin/src/main/kotlin/org/mrseasons/coffeetime/kotlin/app.kt
@@ -0,0 +1,8 @@
+package org.mrseasons.coffeetime.kotlin
+
+/**
+ * Created by mrseasons on 15-1-28.
+ */
+fun main(args: Array<String>) {
+    println("Hello World")
+}
