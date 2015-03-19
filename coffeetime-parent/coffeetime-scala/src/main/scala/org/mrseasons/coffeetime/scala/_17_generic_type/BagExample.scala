ffa56a8ab7c36f21d4a379cfc1da90e75acbff04
sidneyy <siriuseddy@gmail.com>
2015-03-19 18:16:08 +0800

    update scala

--- coffeetime-parent/coffeetime-scala/src/main/scala/org/mrseasons/coffeetime/scala/_17_generic_type/BagExample.scala
+++ coffeetime-parent/coffeetime-scala/src/main/scala/org/mrseasons/coffeetime/scala/_17_generic_type/BagExample.scala
@@ -6,8 +6,12 @@ package org.mrseasons.coffeetime.scala._17_generic_type
 object BagExample {
 
   def main(args: Array[String]) {
-    val s = new EmptyBag[Num].incl(Num(1.0)).incl(Num(2.0))
-    s.contains(Num(1.5))
+val s = new EmptyBag[Num].incl(Num(1.0)).incl(Num(2.0))
+println(s.contains(Num(1.5))) //false
+println(s.contains(Num(1.0))) //true
+
+    // [java.io.File] do not conform to class EmptyBag's type parameter bounds [A <: Ordered[A]]
+    //    val ss=new EmptyBag[File]
 
   }
 }
