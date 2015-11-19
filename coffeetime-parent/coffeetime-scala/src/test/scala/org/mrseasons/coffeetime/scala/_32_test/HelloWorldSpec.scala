4ba0ea03d0538f433b6ace9d970b42601da564be
sidneyy <siriuseddy@gmail.com>
2015-11-19 18:09:43 +0800

    update

--- coffeetime-parent/coffeetime-scala/src/test/scala/org/mrseasons/coffeetime/scala/_32_test/HelloWorldSpec.scala
+++ coffeetime-parent/coffeetime-scala/src/test/scala/org/mrseasons/coffeetime/scala/_32_test/HelloWorldSpec.scala
@@ -1,22 +1,36 @@
-package org.mrseasons.coffeetime.scala
+package org.mrseasons.coffeetime.scala._32_test
 
-import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2._
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.core.SpecStructure
+import specification._
 
 /**
  * Created by mrseasons on 2015/05/25.
  */
-class HelloWorldSpec extends Specification {
-  "This is a specification for the 'Hello world' string".txt
+@RunWith(classOf[JUnitRunner])
+class HelloWorldSpec extends Specification with Groups{
+//  "This is a specification for the 'Hello world' string".txt
 
-  "The 'Hello world' string should" >> {
-    "contain 11 characters" >> {
-      "Hello world" must haveSize(11)
-    }
-    "start with 'Hello'" >> {
-      "Hello world" must startWith("Hello")
-    }
-    "end with 'world'" >> {
-      "Hello world" must endWith("world")
-    }
+//  "The 'Hello world' string should" >> {
+//    "contain 11 characters" >> {
+//      "Hello world" must haveSize(112)
+//    }
+//    "start with 'Hello'" >> {
+//      "Hello world" must startWith("Hello")
+//    }
+//    "end with 'world'" >> {
+//      "Hello world" must endWith("world")
+//    }
+//  }
+
+
+  "hello world" - new group {
+    eg := "Hello world" must haveSize(113)
+    eg := "Hello world" must startWith("Hello")
+    eg := "Hello world" must endWith("world")
   }
+
+  override def is: SpecStructure = "afafafa"
 }
