d85516ea136423105beee76186df82fc122661fd
SidneyXu <siriuseddy@gmail.com>
2015-12-17 17:41:49 +0800

    update

--- coffeetime-parent/coffeetime-groovy/src/main/groovy/org/mrseasons/coffeetime/groovy/_01_hello/hello.groovy
+++ coffeetime-parent/coffeetime-groovy/src/main/groovy/org/mrseasons/coffeetime/groovy/_01_hello/hello.groovy
@@ -1,3 +1,14 @@
 package org.mrseasons.coffeetime.groovy._01_hello
 
 println "Hello World"
+
+
+String str = "\\u7248\\u672c\\u5347\\u7ea7\\u6807\\u9898"
+str = str.replace("\\", "")
+String[] arr = str.split("u")
+StringBuilder builder = new StringBuilder()
+for (int i = 1; i < arr.length; i++) {
+    int hexVal = Integer.parseInt(arr[i], 16);
+    builder.append((char) hexVal)
+}
+System.out.println(builder.toString());
--- coffeetime-parent/coffeetime-java/src/test/java/org/mrseasons/coffeetime/java/Test.java
+++ coffeetime-parent/coffeetime-java/src/test/java/org/mrseasons/coffeetime/java/Test.java
@@ -1,5 +1,8 @@
 package org.mrseasons.coffeetime.java;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,33 +12,50 @@ import java.util.concurrent.Executors;
  */
 public class Test {
 
-    public static void main(String[]args){
-        final ExecutorService service= Executors.newFixedThreadPool(3);
+    public static void main(String[] args) throws UnsupportedEncodingException {
+//        final ExecutorService service= Executors.newFixedThreadPool(3);
+//
+//        for(int i=0;i<10;i++) {
+//            final int j=i;
+//            service.execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    new Thread(() -> {
+//                        long r=new Random().nextInt(10000);
+//                        try {
+//                            Thread.sleep(r);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                        System.out.println("in:"+j+"-"+Thread.currentThread().getName());
+//                    }).start();
+//
+//                    long r=new Random().nextInt(10000);
+//                    try {
+//                        Thread.sleep(r);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println("finish:"+j+"-"+Thread.currentThread().getName());
+//                }
+//            });
+//        }
 
-        for(int i=0;i<10;i++) {
-            final int j=i;
-            service.execute(new Runnable() {
-                @Override
-                public void run() {
-                    new Thread(() -> {
-                        long r=new Random().nextInt(10000);
-                        try {
-                            Thread.sleep(r);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println("in:"+j+"-"+Thread.currentThread().getName());
-                    }).start();
 
-                    long r=new Random().nextInt(10000);
-                    try {
-                        Thread.sleep(r);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("finish:"+j+"-"+Thread.currentThread().getName());
-                }
-            });
+        String str = "android\\u7248\\u672c\\u5347\\u7ea7\\u6807\\u9898";
+        str = str.replace("\\", "");
+        String[] arr = str.split("u");
+        StringBuilder builder = new StringBuilder();
+        for (String s : arr) {
+            try {
+                int hexVal = Integer.parseInt(s, 16);
+                builder.append((char) hexVal);
+            } catch (NumberFormatException e) {
+                builder.append(s);
+            }
         }
+        System.out.println(builder.toString());
+
     }
+
 }
