e1335a4e1d01744a38ae3356c8de955a1ab1c78d
SidneyXu <siriuseddy@gmail.com>
2016-01-05 07:58:40 +0800

    Update.

--- coffeetime-parent/coffetime-ruby/src/if.rb
+++ coffeetime-parent/coffetime-ruby/src/if.rb
@@ -11,3 +11,25 @@ end
 compare(1, 2)
 compare(2, 1)
 compare(2, 2)
+
+
+x = 11
+puts "x>10" if x>10
+puts "x<10" if x<10
+
+unless x >15
+  puts "x<15"
+else
+  puts "x>15"
+end
+
+case x
+  when 0..2
+    puts '0..2'
+  when 3..6
+    puts '3..6'
+  when 7..10
+    puts '7..10'
+  else
+    puts 'other'
+end
