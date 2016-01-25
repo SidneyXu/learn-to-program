4c8cba37726bd2fed9ec72fa4068da6b9c2ee55e
SidneyXu <siriuseddy@gmail.com>
2016-01-26 07:03:17 +0800

    Update.

--- coffeetime-parent/RubyGun/src/22_module.rb
+++ coffeetime-parent/RubyGun/src/22_module.rb
@@ -1,7 +1,17 @@
+# require
+require './utils'
 require './utils'
 
-sentents = ['z', 'b', 'e', 'p']
-# puts StrUtils.sort_words(sentents)
+StrUtils.echo
+puts StrUtils::COUNT
 
+# load './utils.rb'
+# load './utils.rb'
+# StrUtils.echo
+
+# include
 include StrUtils
+# puts echo()
+sentents = ['z', 'b', 'e', 'p']
 puts StrUtils.sort_words(sentents)
+puts sort_words(sentents)
--- coffeetime-parent/RubyGun/src/utils.rb
+++ coffeetime-parent/RubyGun/src/utils.rb
@@ -1,6 +1,14 @@
+puts 'load module'
+COUNT = 10
 module StrUtils
+  COUNT = 20
+
   def sort_words(words)
     words.sort
+  end
+
+  def self.echo
+    puts "#{COUNT} #{::COUNT}"
   end
 end
 
