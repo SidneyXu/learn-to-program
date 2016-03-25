82a596c2d7e2f8cb68f25dff71bc1f6a36e90b73
SidneyXu <siriuseddy@gmail.com>
2016-03-25 08:19:09 +0800

    Update.

---
+++ coffeetime-parent/NodeGun/es6/08.iterator.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mrseasons on 16/3/24.
+ */
+'use strict';
+
+const colors = ['red', 'green', 'blue'];
+let iterator = colors[Symbol.iterator]();
+
+for (let v of iterator) {
+    console.log(v); // red green blue
+}
+
+for (let i in colors) {
+    console.log(i); // 0 1 2
+}
+
+for (let v of colors) {
+    console.log(v); // red green blue
+}
