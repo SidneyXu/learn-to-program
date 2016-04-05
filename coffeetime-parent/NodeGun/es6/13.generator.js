6d97e60a444d4badee37412e0833f80d7ed7eaf1
SidneyXu <siriuseddy@gmail.com>
2016-04-05 08:53:02 +0800

    Update.

--- coffeetime-parent/NodeGun/es6/13.generator.js
+++ coffeetime-parent/NodeGun/es6/13.generator.js
@@ -14,3 +14,22 @@ console.log(hw.next()); //  { value: 'hello', done: false }
 console.log(hw.next()); //  { value: 'world', done: false }
 console.log(hw.next()); //  { value: 'ending', done: true }
 console.log(hw.next()); //  { value: undefined, done: true }
+ 
+
+function showLoadingScreen() {
+    console.log('loading...');
+}
+function hideLoadingScreen() {
+    console.log('dismiss loading...');
+}
+function loadUIDataAsynchronously() {
+    console.log('load data');
+}
+function* loadUI() {
+    showLoadingScreen();
+    yield loadUIDataAsynchronously();
+    hideLoadingScreen();
+}
+var loader = loadUI();
+loader.next();
+loader.next();
