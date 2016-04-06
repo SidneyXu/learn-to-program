5783fef8967df9edc69a8e75528551db23f250bf
SidneyXu <siriuseddy@gmail.com>
2016-04-06 08:23:33 +0800

    Update.

---
+++ coffeetime-parent/Python3Gun/src/03_console.py
@@ -0,0 +1,6 @@
+# Console input/output
+age = input('How old are you? ')
+height = input('How tall are you? ')
+weight = input('How much do you weigh? ')
+
+print("So, you're {age} old, {height} tall and {weight} heavy.".format(age=age, height=height, weight=weight))
---
+++ coffeetime-parent/Python3Gun/src/06_variable.py
@@ -0,0 +1,13 @@
+foo = 'bar'
+BAR = 'bar'
+
+foo = 123
+BAR = 456
+
+print(foo)
+print(BAR)
+
+one = 1
+two = 2
+one, two = 2, 1
+print(one, two)
